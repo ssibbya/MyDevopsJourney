daf7e61026bff496c4a2180694bfd8a6cee26bd7
ssibbya <ssibbya@gmail.com>
2025-04-05 23:56:00 +0530

    Add files via upload

---
+++ Linux-ShellScripting/crontabProject/aws-resource-report.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+##############################
+#Author: Sibbya
+#Date: 05/04/25
+#Version: v1
+#Generate a report on list of EC2,S3,IAM,Lambda
+##############################
+
+LOGFILE="/home/ubuntu/aws-report.log"
+TO="ssibbya@gmail.com"
+FROM="MS_g4PSGw@test-ywj2lpnq5pmg7oqz.mlsender.net"
+
+>"$LOGFILE"
+
+#set -x
+
+echo "List of EC2 Instances" >> "$LOGFILE"
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' >> "$LOGFILE"
+
+echo "List of S3 Buckets" >> "$LOGFILE"
+aws s3 ls >> "$LOGFILE"
+
+echo "List of IAM Users" >> "$LOGFILE"
+aws iam list-users | jq '.Users[].UserName' >> "$LOGFILE"
+
+echo "List of Lambda functions" >> "$LOGFILE"
+aws lambda list-functions | jq '.Functions[].FunctionName' >> "$LOGFILE"
+
+(
+  echo "Subject: AWS Weekend Resource Report"
+  echo "To: $TO"
+  echo "From: $FROM"
+  echo
+  cat "$LOGFILE"
+) | msmtp "$TO"
