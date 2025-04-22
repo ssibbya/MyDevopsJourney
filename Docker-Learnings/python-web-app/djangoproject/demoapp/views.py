072cd156d266be569e9d86227148cc4eea82c748
Ubuntu <ubuntu@ip-172-31-84-185.ec2.internal>
2025-04-22 11:12:52 +0000

    my-pyton-app

--- Docker-Learnings/python-web-app/djangoproject/demoapp/views.py
+++ Docker-Learnings/python-web-app/djangoproject/demoapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
+def welcome_view(request):
+    return render(request, 'demoapp/welcome.html')
--- Docker-Learnings/python-web-app/djangoproject/djangoproject/settings.py
+++ Docker-Learnings/python-web-app/djangoproject/djangoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+88fu^&)0p+jc3bcr4wu$dsktjy^47*rp$0#6l(bsvsqdi*jq4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'demoapp',
 ]
 
 MIDDLEWARE = [
